1ebd39c976e815dc8981dea9c226581d0f223f99
Rizwan <112270212+sial059689@users.noreply.github.com>
2024-01-12 00:00:54 +0500

    Add files via upload

---
+++ SpeakGoodBye.js
@@ -0,0 +1,5 @@
+var names = ["Jason"];
+console.log("Goodbye " + names);
+
+var names = ["Larry"];
+console.log("Hello " + names);
---
+++ SpeakHello.js
@@ -0,0 +1,5 @@
+var names = ["Jen"];
+console.log("Goodbye " + names);
+
+var names = ["Frank"];
+console.log("Hello " + names);
